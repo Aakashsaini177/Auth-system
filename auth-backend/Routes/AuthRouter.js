9626f200ba4fe4997b45be26f8d0d8541ce96640
Aakash Saini <sainiaakash177@gmail.com>
2025-11-03 09:28:02 +0530

    Fixed AuthController import issue and case sensitivity

--- auth-backend/Routes/AuthRouter.js
+++ auth-backend/Routes/AuthRouter.js
@@ -1,5 +1,5 @@
+import { registerUser, loginUser } from "../Controllers/AuthController";
 import express from "express";
-import { registerUser, loginUser } from "../controllers/authController.js";
 const router = express.Router();
 
 router.post("/signup", registerUser);
